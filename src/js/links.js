eab4c6d9f9f2b48112e4e7d892b5d678addc8e83
Jonathan Kelly <jskva@jskva.com>
2025-03-07 23:30:07 -0800

    Improve URL regex

--- src/js/links.js
+++ src/js/links.js
@@ -1,15 +1,17 @@
+function removeHtmlTags(input) {
+    return input.replace(/<\/?(?:em|strong)>/g, '');
+}
+
 export function replaceLinks(el) {
-    const urlPattern = /((?:https?:\/\/)?(www\.)?[-a-zA-Z0-9.]{1,256}\.[a-zA-Z][a-zA-Z0-9]{1,5}\b([-a-zA-Z0-9()@:%_+.~#?&/=;]*[a-zA-Z0-9_#])?)/gi;
+    const urlPattern = /\b((?:https?:\/\/)?(?:[a-zA-Z0-9-]+\.)+(?:com|org|net|gov|edu|io|co|dev|us|uk|ca|au|de|fr|jp|ru|cn|be|tv)(?:\/(?:[^\s<]|<\/?(?:em|strong)>)*)?(?:\?(?:[^\s<]|<\/?(?:em|strong)>)*)?(?:#(?:[^\s<]|<\/?(?:em|strong)>)*)?)\b/g;
     el.innerHTML = el.innerHTML.replace(urlPattern, function (match, url) {
-        const href = url.indexOf("://") > 0 ? url : 'https://' + url;
+        const cleanedUrl = removeHtmlTags(url);
+        const href = cleanedUrl.indexOf("://") > 0 ? cleanedUrl : 'https://' + cleanedUrl;
         try {
             // Some things might look like a URL, according to the rudimentary regex above,
             // but we confirm here that it is in fact a valid URL.
-            // One thing in particular that we don't want to match (but that still matches the
-            // above regex) is something like "foo...bar".
-            if (!new URL(href).hostname.includes('..')) {
-                return `<a href="${href}" target="_blank">${url}</a>`;
-            }
+            new URL(href);
+            return `<a href="${href}" target="_blank">${cleanedUrl}</a>`;
         } catch (_) {
         }
 
